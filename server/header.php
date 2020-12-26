f327501510ac70228a5d96ffad749777c8761d09
Riverside Rocks <trent@riverside.rocks>
2020-12-26 18:47:58 -0500

    Header

--- server/header.php
+++ server/header.php
@@ -41,3 +41,84 @@ if(isset($title) && isset($desc) && isset($thumb) && isset($route))
 ?>
 <meta property="og:type" content="website">
 <meta property="twitter:card" content="summary_large_image">
+
+<body>
+  <div class="page-wrapper with-navbar">
+    <nav class="navbar">
+      <div class="navbar-content">
+        <button class="btn btn-action" type="button">
+          <i class="fa fa-bars" aria-hidden="true"></i>
+          <span class="sr-only">Menu</span> <!-- sr-only = show only on screen readers -->
+        </button>
+      </div>
+      <a href="/" class="navbar-brand">
+        Flag
+      </a>
+      <span class="navbar-text text-monospace">Beta</span>
+      <ul class="navbar-nav d-none d-md-flex">
+        <li class="nav-item active">
+          <a href="/top" class="nav-link">Top</a>
+        </li>
+        <li class="nav-item">
+          <a href="/new" class="nav-link">New</a>
+        </li>
+      </ul>
+      <?php
+      if(!isset($_SESSION["username"]))
+      {
+          echo "<form class='form-inline d-none d-md-flex ml-auto' action='/accounts/create' method='get'>";
+          echo '<input type="text" class="form-control" placeholder="Create a username" required="required">';
+          echo '<button class="btn btn-primary" type="submit">Sign up</button>';
+          echo '</form>';
+      }else{
+        echo 
+        "
+        <li class='nav-item'>
+          <a href='/account/home' class='nav-link'> " . $_SESSION["username"] . "</a>
+        </li>
+        ";
+      }
+      ?>
+        
+      <div class="navbar-content d-md-none ml-auto"> <!-- d-md-none = display: none on medium screens and up (width > 768px), ml-auto = margin-left: auto -->
+        <div class="dropdown with-arrow">
+          <button class="btn" data-toggle="dropdown" type="button" id="navbar-dropdown-toggle-btn-1">
+            Menu
+            <i class="fa fa-angle-down" aria-hidden="true"></i>
+          </button>
+          <div class="dropdown-menu dropdown-menu-right w-200" aria-labelledby="navbar-dropdown-toggle-btn-1"> <!-- w-200 = width: 20rem (200px) -->
+            <a href="/top" class="dropdown-item">Top</a>
+            <a href="/new" class="dropdown-item">New</a>
+            <div class="dropdown-divider"></div>
+            <div class="dropdown-content">
+            <?php
+            if(!isset($_SESSION["username"]))
+            {
+                echo "<form action='/accounts/create' method='get'>";
+                echo '<div class="form-group"><input type="text" class="form-control" placeholder="Create a username" required="required"></div>';
+                echo '<button class="btn btn-primary btn-block" type="submit">Sign up</button>';
+                echo '</form>';
+            }else{
+                echo 
+                "
+                <li class='nav-item'>
+                <a href='/account/home' class='dropdown-item'> " . $_SESSION["username"] . "</a>
+                </li>
+                ";
+            }
+            ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Content wrapper -->
+    <div class="content-wrapper">
+      ...
+    </div>
+  </div>
+
+  <!-- Requires halfmoon.js for the dropdowns -->
+  <script src="path/to/halfmoon.js"></script>
+</body>
--- server/home.php
+++ server/home.php
@@ -1,2 +1,3 @@
 <?php 
 include "header.php";
+
