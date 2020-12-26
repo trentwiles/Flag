9320b066c759a4abdcc5ca7ae8d837ffb207d8d4
Riverside Rocks <trent@riverside.rocks>
2020-12-26 17:43:32 -0500

    Setup

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+header("X-Powered-By: Riverside Rocks");
+header("X-Server: kestral (v2.2)");
+header("X-Content-Type-Options: nosniff");
+header("X-XSS-Protections: 1; mode=block");
+header("X-Frame-Options: SAMEORIGIN");
+
+require __DIR__ . "/vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+ob_start();
+
+$router = new \Bramus\Router\Router();
+
+$router->get('/', function() {
+    include "server/home.php";
+    $out = ob_get_clean();
+    die($out);
+});
