8c36680429367d6809100974ed9d9f63efe1315e
Riverside Rocks <trent@riverside.rocks>
2020-12-27 09:15:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -62,4 +62,13 @@ $router->get('/upload', function() {
     require "server/upload.php";
 });
 
+$router->mount('/account', function () use ($router) {
+    $router->get('/', function () {
+        die(header("Location: /account/home"));
+    });
+    $router->post('/home', function() {
+        require "server/account.php";
+    });
+});
+
 $router->run();
