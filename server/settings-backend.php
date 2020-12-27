d9a44c6fe48a9c9fccf1ac1a883bb221c7ceaa6e
Riverside Rocks <trent@riverside.rocks>
2020-12-27 17:38:14 -0500

    Update settings-backend.php

--- server/settings-backend.php
+++ server/settings-backend.php
@@ -23,45 +23,47 @@ if(isset($_POST["bio"]))
     $new_bio = htmlspecialchars($_POST["bio"]);
     $stmt->bind_param("ss", $new_bio, $_SESSION["username"]);
     $stmt->execute();
+    die(header("Location: /account/"));
 }
 
 
-    $storage = new \Upload\Storage\FileSystem('/var/www/drive1/cdn/profiles/');
-        $file = new \Upload\File('foo', $storage);
+$storage = new \Upload\Storage\FileSystem('/var/www/drive1/cdn/profiles/');
+$file = new \Upload\File('foo', $storage);
 
-        $names = json_decode(file_get_contents("https://friendlywords.eddiestech.co.uk/.netlify/functions/words/objects"), true);
+$names = json_decode(file_get_contents("https://friendlywords.eddiestech.co.uk/.netlify/functions/words/objects"), true);
 
 
-        $new_filename = $names[0] . "-" . $names[1] . "-" . $names[2];
-        $file->setName($new_filename);
+$new_filename = $names[0] . "-" . $names[1] . "-" . $names[2];
+$file->setName($new_filename);
 
-        $file->addValidations(array(
+$file->addValidations(array(
 
-            new \Upload\Validation\Mimetype(array('image/png', 'image/jpg', 'image/jpeg')),
+    new \Upload\Validation\Mimetype(array('image/png', 'image/jpg', 'image/jpeg')),
 
-            new \Upload\Validation\Size('5M')
-                ));
+    new \Upload\Validation\Size('5M')
+        ));
 
-                // Access data about the file that has been uploaded
-                $data = array(
-                    'name'       => $file->getNameWithExtension(),
-                    'extension'  => $file->getExtension(),
-                    'mime'       => $file->getMimetype(),
-                    'size'       => $file->getSize(),
-                    'md5'        => $file->getMd5(),
-                    'dimensions' => $file->getDimensions()
-                );
+        // Access data about the file that has been uploaded
+        $data = array(
+            'name'       => $file->getNameWithExtension(),
+            'extension'  => $file->getExtension(),
+            'mime'       => $file->getMimetype(),
+            'size'       => $file->getSize(),
+            'md5'        => $file->getMd5(),
+            'dimensions' => $file->getDimensions()
+        );
 
-                // Try to upload file
-                try {
-                    $file->upload();
-                    $sql = "UPDATE `users` SET `pfp`=? WHERE username=?";
-                    $stmt = $conn->prepare($sql); 
-                    $new_photo = "https://cdn.riverside.rocks/profiles/" . $file->getNameWithExtension();
-                    $stmt->bind_param("ss", $new_photo, $_SESSION["username"]);
-                    $stmt->execute();
-                } catch (\Exception $e) {
-                    // Fail!
-                    $errors = $file->getErrors();
-                }
+        // Try to upload file
+        try {
+            $file->upload();
+            $sql = "UPDATE `users` SET `pfp`=? WHERE username=?";
+            $stmt = $conn->prepare($sql); 
+            $new_photo = "https://cdn.riverside.rocks/profiles/" . $file->getNameWithExtension();
+            $stmt->bind_param("ss", $new_photo, $_SESSION["username"]);
+            $stmt->execute();
+            die(header("Location: /account/"));
+        } catch (\Exception $e) {
+            // Fail!
+            $errors = $file->getErrors();
+        }
             
