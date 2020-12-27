f9ca9fbf61200a7e746b5470872ff5e58945348d
Riverside Rocks <trent@riverside.rocks>
2020-12-27 13:26:03 -0500

    Update user.php

--- server/user.php
+++ server/user.php
@@ -17,14 +17,18 @@ $stmt->bind_param("s", $useri);
 $stmt->execute();
 $result = $stmt->get_result();
 while ($row = $result->fetch_assoc()) {
-    
+    if(!isset($row["username"]))
+    {
+        die("<h1>404: Not Found</h1>");
+    }
+    $use = $row["username"];
 }
 
 ?>
 
 <div class="content">
   <h2 class="content-title">
-    It was going to rain
+    <?php echo $use; ?>
   </h2>
   <p>
     The weather forecast didn't say that, but the steel plate in his hip did. He had learned over the years to trust his hip over the weatherman. It was going to rain, so he better get outside and prepare. Sleeping in his car was never the plan but sometimes things don't work out as planned. This had been his life for the last three months and he was just beginning to get used to it. He didn't actually enjoy it, but he had accepted it and come to terms with it. Or at least he thought he had. All that changed when he put the key into the ignition, turned it and the engine didn't make a sound.
