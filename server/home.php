baff807fdca384f8e12472b819f9507556a234a7
Riverside Rocks <trent@riverside.rocks>
2020-12-27 12:53:38 -0500

    Update home.php

--- server/home.php
+++ server/home.php
@@ -1,6 +1,16 @@
 <?php 
 include "header.php";
 
+$servername = $_ENV['MYSQL_SERVER'];
+$username = $_ENV["MYSQL_USERNAME"];
+$password = $_ENV["MYSQL_PASSWORD"];
+$dbname = $_ENV["MYSQL_DATABASE"];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $sql = "SELECT * FROM videos";
 $stmt = $conn->prepare($sql); 
 $stmt->execute();
