865af55480bc186245cc3e9b9d20759446fda25c
Riverside Rocks <trent@riverside.rocks>
2020-12-27 15:50:27 -0500

    fix prep

--- server/signup-service.php
+++ server/signup-service.php
@@ -53,7 +53,7 @@ if(isset($_POST["username"]) && $_POST["password"])
               $new_id = rand();
               $san_email = htmlspecialchars($_POST["email"]);
               $secure_pass = password_hash($_POST["password"], PASSWORD_DEFAULT);
-              $stmt = $conn->prepare("INSERT INTO users (username, `password`, email, signup_time, id, ip, bio) VALUES (?, ?, ?, ?, ?, ?)");
+              $stmt = $conn->prepare("INSERT INTO users (username, `password`, email, signup_time, id, ip, bio) VALUES (?, ?, ?, ?, ?, ?, ?)");
               $cbio = "A fine user!";
               $stmt->bind_param("sssisss", $prep_name, $secure_pass, $san_email, $sign_time, $new_id, $sign_ip, $cbio);
               $stmt->execute();
