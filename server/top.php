5e967e6e08c6c6167a5343878ebd7bb76d35bea0
Riverside Rocks <trent@riverside.rocks>
2020-12-27 20:10:05 -0500

    Update top.php

--- server/top.php
+++ server/top.php
@@ -12,27 +12,43 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM stat ORDER BY views DESC";
+$sql = "SELECT * FROM stat ORDER BY views DESC LIMIT 15";
 $stmt = $conn->prepare($sql); 
 $stmt->execute();
 $result = $stmt->get_result();
-$count = 0;
 
-echo '<br><div class="container-fluid"><div class="row">';
+$top = array();
 
 while ($row = $result->fetch_assoc()) {
-    $count++;
-    if($count == 4 || $count == 8 || $count == 12)
-    {
-        echo '<div class="container-fluid"><div class="row">';
-    }
-    echo '<div class="col-sm">';
-    echo "<a href='/watch/" . $row["v_id"] . "'><img src='" . $row["v_thumb"] . "' height='144px' width='360'/></a>";
-    echo "<br><p>" . $row["v_title"] . "</p>";
-    echo "</div>";
-    if($count == 4 || $count == 8 || $count == 12)
-    {
-        echo "</div></div>";
-    }
+    array_push($top, $row["id"]);
 }
-echo "</div></div>";
+
+foreach($top as $video)
+{
+    $sql = "SELECT * FROM videos WHERE v_id=?";
+    $stmt = $conn->prepare($sql); 
+    $stmt->bind_param("s", $video);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $count = 0;
+
+    echo '<br><div class="container-fluid"><div class="row">';
+
+    while ($row = $result->fetch_assoc()) {
+        $count++;
+        if($count == 4 || $count == 8 || $count == 12)
+        {
+            echo '<div class="container-fluid"><div class="row">';
+        }
+        echo '<div class="col-sm">';
+        echo "<a href='/watch/" . $row["v_id"] . "'><img src='" . $row["v_thumb"] . "' height='144px' width='360'/></a>";
+        echo "<br><p>" . $row["v_title"] . "</p>";
+        echo "</div>";
+        if($count == 4 || $count == 8 || $count == 12)
+        {
+            echo "</div></div>";
+        }
+    }
+    echo "</div></div>";
+
+}
