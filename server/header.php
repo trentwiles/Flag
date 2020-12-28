161a56208582e43c44cf2cc0bb9e636b1b9f0fba
Riverside Rocks <trent@riverside.rocks>
2020-12-28 10:00:19 -0500

    Jquery

--- server/header.php
+++ server/header.php
@@ -12,6 +12,7 @@
     </title>
     <link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/js/halfmoon.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </head>
 <?php
 if(isset($title) && isset($desc) && isset($thumb) && isset($route))
