c07666f0d2344bce58e303d36b7349e2befcc71c
Riverside Rocks <trent@riverside.rocks>
2020-12-29 16:31:54 -0500

    video api

--- frontend/username.js
+++ frontend/username.js
@@ -1,6 +1,7 @@
 function check()
 {
-    document.getElementById("username").addEventListener("input", () => {
+    var ele = document.getElementById("username")
+    ele.addEventListener("input", () => {
         $.get("/api/v1/username?username="+document.getElementById("write").value, function(data, status){
             if(data.username == "")
             {
