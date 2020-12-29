1c0d3913c1bfc4c4f1104697087dba0a8e6ef0eb
Riverside Rocks <trent@riverside.rocks>
2020-12-29 14:46:24 -0500

    user api

--- api/user.php
+++ api/user.php
@@ -24,3 +24,33 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$request = $_GET["username"];
+
+if(! $request)
+{
+    header("HTTP/1.1 400 Bad Request");
+    die(json_encode(array("success" => "false", "message" => "400 Bad Request")));
+}
+
+$sql = "SELECT * FROM users WHERE `username`=?";
+$stmt = $conn->prepare($sql); 
+$stmt->bind_param("s", $request);
+$stmt->execute();
+$result = $stmt->get_result();
+while ($row = $result->fetch_assoc()) {
+    if(! $row["username"])
+    {
+        die(json_encode(array("message" => "No such user")));
+    }
+    $details = array(
+        "username" => $row["username"],
+        "signup_time" => $row["signup_time"],
+        "id" => $row["id"],
+        "bio" => $row["bio"],
+        "profile_photo" => $row["pfp"]
+    );
+    $final = array(
+        "details" => $details
+    );
+    die(json_encode($final));
+}
