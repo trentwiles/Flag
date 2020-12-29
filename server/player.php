e5c516ee5df86580c168d145fb27c4c86d8213e6
Riverside Rocks <trent@riverside.rocks>
2020-12-29 10:14:14 -0500

    Take 2 on comments section

--- server/player.php
+++ server/player.php
@@ -109,5 +109,5 @@ $stmt->bind_param("s", $req);
 $stmt->execute();
 $result = $stmt->get_result();
 while ($row = $result->fetch_assoc()) {
-    echo "<br><a href='/user/" . $row["username"] . "'> - " . $row["comment"] . "<hr>";
+    echo "<br><h4><a href='/user/" . $row["username"] . "'>" . $row["username"] . "</a> -" . $row["comment"] . "</h4><hr>";
 }
