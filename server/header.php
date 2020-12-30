cb12cfe17211557933f7954925b15dadcbe6313d
Riverside Rocks <trent@riverside.rocks>
2020-12-30 08:59:33 -0500

    dynamic title

--- server/header.php
+++ server/header.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <head>
-    <title>
-    <?php
-    if(isset($title))
-    {
-        echo $title . " - Flag";
-    }else{
-        echo "Flag - Bite Size Videos";
-    }
-    ?>
-    </title>
+    
     <link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/js/halfmoon.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -24,6 +15,14 @@ if(isset($req))
   $thumb = $api["details"]["thumbnail"];
   $route = "watch/${req}";
 }
+echo "<title>";
+    if(isset($title))
+    {
+        echo $title . " - Flag";
+    }else{
+        echo "Flag - Bite Size Videos";
+    }
+echo "    </title>";
 if(isset($title) && isset($desc) && isset($thumb) && isset($route))
 {
     echo "<meta name='title' content='${title} - Flag'>";
