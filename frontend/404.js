fe2a8a5e5ce0bd3f3112c76881f55286c1f7830d
Riverside Rocks <trent@riverside.rocks>
2020-12-30 16:46:12 -0500

    Update 404.js

--- frontend/404.js
+++ frontend/404.js
@@ -1,7 +1,10 @@
-$.get("/api/v1/videos?id="+id, function(data, status){
-    if(data.details.title == "")
-    {
-        window.location = "/404";
-    }
-    console.log("status");
-});
+function check()
+{
+    $.get("/api/v1/videos?id="+id, function(data, status){
+        if(data.details.title == "")
+        {
+            window.location = "/404";
+        }
+        console.log("status");
+    });
+}
