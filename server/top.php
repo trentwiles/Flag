f5097c42cad6d881653b57b04c9766115cdbfb17
Riverside Rocks <trent@riverside.rocks>
2020-12-30 16:58:15 -0500

    Cleanup

--- server/top.php
+++ server/top.php
@@ -12,43 +12,62 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM stat ORDER BY views DESC LIMIT 15";
+$sql = "SELECT * FROM stat ORDER BY views DESC LIMIT 20";
 $stmt = $conn->prepare($sql); 
 $stmt->execute();
 $result = $stmt->get_result();
+$a = 0;
+while ($row = $result->fetch_assoc()) {
+    $a++;
+}
 
+$sql = "SELECT * FROM stat ORDER BY views DESC LIMIT 20";
+$stmt = $conn->prepare($sql); 
+$stmt->execute();
+$result = $stmt->get_result();
 $top = array();
-
+$views = array();
 while ($row = $result->fetch_assoc()) {
     array_push($top, $row["id"]);
+    array_push($views, $row["views"]);
 }
 
-foreach($top as $video)
+echo "<!--";
+print_r($top);
+echo "-->";
+
+echo "<h1>Flag - Bite Sized Videos</h1>";
+echo "<script src='/frontend/top.js'></script>";
+
+
+echo '<br><div class="container-fluid"><div class="row">';
+$count = 0;
+foreach($top as $vias)
 {
+    $count++;
+    if($count == 4 || $count == 8 || $count == 12)
+    {
+        echo '<div class="container-fluid"><div class="row">';
+    }
     $sql = "SELECT * FROM videos WHERE v_id=?";
     $stmt = $conn->prepare($sql); 
-    $stmt->bind_param("s", $video);
+    $stmt->bind_param("s", $vias);
     $stmt->execute();
     $result = $stmt->get_result();
-    $count = 0;
-
-    echo '<br><div class="container-fluid"><div class="row">';
-
+    $top = array();
     while ($row = $result->fetch_assoc()) {
-        $count++;
-        if($count == 4 || $count == 8 || $count == 12)
-        {
-            echo '<div class="container-fluid"><div class="row">';
-        }
-        echo '<div class="col-sm">';
-        echo "<a href='/watch/" . $row["v_id"] . "'><img src='" . $row["v_thumb"] . "' height='144px' width='360'/></a>";
-        echo "<br><p>" . $row["v_title"] . "</p>";
-        echo "</div>";
-        if($count == 4 || $count == 8 || $count == 12)
-        {
-            echo "</div></div>";
-        }
+        $thumb = $row["v_thumb"];
+        $title = $row["v_title"];
+    
+    echo '<div class="col-sm">';
+    echo "<a href='/watch/" . $row["v_id"] . "'><img src='" . $row["v_thumb"] . "' height='144px' width='360'/></a>";
+    echo "<br><p>" . $row["v_title"] . "</p>";
+    echo "</div>";
+    if($count == 4 || $count == 8 || $count == 12)
+    {
+        echo "</div></div>";
     }
-    echo "</div></div>";
-
+    
 }
+}
+echo "</div></div>";
