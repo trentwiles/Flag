a9688c8ae2a5d85eae7c477d3ac3974579cb8c41
RiversideRocks <wilest7@gmail.com>
2021-01-18 19:50:55 -0500

    edit page

--- server/account-edit.php
+++ server/account-edit.php
@@ -25,6 +25,8 @@ $stmt->bind_param("s", $video_id);
 $stmt->execute();
 $result = $stmt->get_result();
 
+echo '<form method="post" class="w-400 mw-full">';
+
 while ($row = $result->fetch_assoc()) {
     if($row["v_uploader"] !== $_SESSION["username"])
     {
@@ -32,4 +34,42 @@ while ($row = $result->fetch_assoc()) {
     }
     $cur_title = $row["v_title"];
     $cur_desc = $row["v_desc"];
+    echo '<div class="form-group">';
+    echo "<label for='title' class='required'>Title</label>";
+    echo "<input type='text' class='form-control' id='title' placeholder='${cur_title}' required='required'>";
+    echo "</div>";
+    echo '<div class="form-group">';
+    echo '<label for="description">Description</label>';
+    echo "<textarea class='form-control' id='description' placeholder='${cur_desc}' required='required'>";
+    echo "</div>";
+    echo '<input class="btn btn-primary" type="submit" value="Submit"></form>';
+    break;
+}
+
+if(isset($_POST["title"]) && isset($_POST["description"]))
+{
+    $sql = "SELECT * FROM videos WHERE v_id=?"; // limited to 20 for now
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $video_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    echo '<form method="post" class="w-400 mw-full">';
+
+    while ($row = $result->fetch_assoc()) {
+        if($row["v_uploader"] !== $_SESSION["username"])
+        {
+            die("<br><h2>Looks like you don't have access to this video or it doesn't exsist.</h2>");
+        }
+    }
+    
+    $new_title = htmlspecialchars($_POST["title"]);
+    $new_description = htmlspecialchars($_POST["description"]);
+
+    $sql = "SELECT * FROM videos WHERE v_id=?"; // limited to 20 for now
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $video_id);
+    $stmt->execute();
 }
