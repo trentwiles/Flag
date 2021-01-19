a149c51c7a6e5fb73dbc353abe8cda2dea6496b0
RiversideRocks <wilest7@gmail.com>
2021-01-19 14:26:55 -0500

    Add about page

---
+++ about/about.php
@@ -0,0 +1,18 @@
+<?php 
+include "header.php";
+
+?>
+<h5 class="sidebar-title">About Us</h5>
+<div class="sidebar-divider"></div>
+<a href="#about" class="sidebar-link active">What is Flag?</a>
+<a href="#faq" class="sidebar-link">FAQ</a>
+<br />
+<h5 class="sidebar-title">Open Source</h5>
+<div class="sidebar-divider"></div>
+<a href="/about/docs/" class="sidebar-link">Getting Started</a>
+<a href="/about/docs/api/" class="sidebar-link">API Docs</a>
+
+<h1 id="about">Welcome to Flag!</h1>
+<p>Flag is a great open source video sharing website that you never knew you needed.</p>
+
+<?php
