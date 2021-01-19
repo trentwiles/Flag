b2dfbbf40062d5a6b14099bc615fca86fdfc212f
RiversideRocks <wilest7@gmail.com>
2021-01-18 21:00:16 -0500

    delete a video (CHORE: add delete endpoint)

--- frontend/delete.js
+++ frontend/delete.js
@@ -1,4 +1,36 @@
 function areYouSure(id)
 {
-    console.log("Deleting is a beta feature. (in context to video ID "+id+")");
+    Swal.fire({
+        title: 'Are you sure want to delete this video?',
+        text: "There is no turning back!!!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: "I'm sure"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            $.post("/api/v1/delete",
+            {
+            id: id,
+            },
+            function(data,status){
+                if(data.message !== "success")
+                {
+                    Swal.fire(
+                        'Error!',
+                        'Something went wrong. Make sure you have permission to delete this video.',
+                        'error'
+                      )
+                }else{
+                    Swal.fire(
+                        'Video Removed',
+                        'Your video has been deleted. ('+status+')',
+                        'success'
+                      )
+                }
+            });
+          // delete
+        }
+      })
 }
