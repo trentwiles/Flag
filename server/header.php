933c4aa58b684cb5106c25c1c7c73a115c53bde7
RiversideRocks <wilest7@gmail.com>
2021-01-18 21:01:34 -0500

    add swal js

--- server/header.php
+++ server/header.php
@@ -5,6 +5,7 @@
     <script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/js/halfmoon.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/281a5c53f1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </head>
 <?php
 if(isset($req))
