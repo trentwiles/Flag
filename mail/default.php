8426c364c35d7fa03e316fe645cbb39a6f36e6f8
RiversideRocks <wilest7@gmail.com>
2021-01-20 10:08:07 -0500

    Update default.php

--- mail/default.php
+++ mail/default.php
@@ -2,6 +2,9 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
