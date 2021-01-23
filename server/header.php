49fb7decd0438053424c7f26928a9b610c1a33f5
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2021-01-23 10:29:21 -0500

    Remove defresh for now

--- server/header.php
+++ server/header.php
@@ -6,7 +6,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/281a5c53f1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script src="https://cdn.jsdelivr.net/gh/DerDer56/defresh/defresh.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/gh/DerDer56/defresh/defresh.js"></script> -->
 </head>
 <?php
 if(isset($req))
