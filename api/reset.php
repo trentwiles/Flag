63eacc00a37b809581ce231bc47b9a1f07888991
RiversideRocks <wilest7@gmail.com>
2021-01-26 14:03:02 -0500

    shut off debug

--- api/reset.php
+++ api/reset.php
@@ -41,7 +41,7 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
