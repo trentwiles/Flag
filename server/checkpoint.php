88a4e8d59e07c83431b559a8f8ca4dde8784f057
RiversideRocks <wilest7@gmail.com>
2021-02-11 12:07:15 -0500

    fix

---
+++ server/checkpoint.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+
+Service to prevent scraping/malicous traffic w/ abuseipdb
+
+*/
+?>
+<html>
+<head><meta http-equiv="content-type" content="text/html; charset=utf-8"><meta name="viewport" content="initial-scale=1"><title>Checkpoint</title></head>
+<body style="font-family: arial, sans-serif; background-color: #fff; color: #000; padding:20px; font-size:18px;" onload="e=document.getElementById('captcha');if(e){e.focus();}">
+<div style="max-width:400px;">
+  <script src="https://www.hCaptcha.com/1/api.js" async defer></script>
+  <div class="h-captcha" data-sitekey="80228737-9c23-4e37-a270-2cc47cca9fbe"></div>
+<hr noshade size="1" style="color:#ccc; background-color:#ccc;"><br>
+<div style="font-size:13px;">
+Our systems have detected unusual traffic from your IP address (<?php htmlspecialchars($_SERVER["REMOTE_ADDR"]); ?>).  Please try your request again later. <br><br>
+
+
+IP address: <?php htmlspecialchars($_SERVER["REMOTE_ADDR"]); ?><br>Time: <?php echo htmlspecialchars(time()); ?><br> <?php "Request Error ID: " . random_bytes(hex2bin()); ?>
+</div>
+</div>
+</body>
+</html>
